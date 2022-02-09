967bc7ac0f7a62a9abffb72773821a25d6c1ab0b
Leo Neto <oleoneto@gmail.com>
2022-02-08 20:56:18 -0400

    fix: remove router_name in devise config

--- lib/polivalente/version.rb
+++ lib/polivalente/version.rb
@@ -1,3 +1,3 @@
 module Polivalente
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: "auth"
 
   mount Polivalente::Engine => "/"
 end
