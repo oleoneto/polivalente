b47a0a4703326a97b117e4dd22db183a09a23048
Leo Neto <oleoneto@gmail.com>
2022-03-07 18:05:26 -0400

    feat(controllers): updates users#show and set_user action

--- app/controllers/polivalente/users_controller.rb
+++ app/controllers/polivalente/users_controller.rb
@@ -6,6 +6,14 @@ module Polivalente
     end
 
     def show
+      @recent_activities = []
+
+      # Sudo-random list of user suggestions
+      @related_users = @user_class.all
+        .where(id: 10.times.map { rand(@user_class.first.id...@user_class.last.id) })
+        .where.not(id: @user.id)
+        .latest
+        .first(4)
     end
 
     def edit
@@ -22,9 +30,8 @@ module Polivalente
     private
 
     def set_user
-      user_class = Polivalente.config.user_class.constantize
-
-      @user = user_class.find(params[:id])
+      @user_class = Polivalente.config.user_class.constantize
+      @user = @user_class.find(params[:id])
       @page_title = @user.name
     end
 
