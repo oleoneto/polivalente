6e070f1658786646c77eb2708f017faca5bf7825
Leo Neto <oleoneto@gmail.com>
2022-03-07 17:54:22 -0400

    feat(models): adds ActionText::Attachable

--- lib/generators/polivalente/templates/user.rb
+++ lib/generators/polivalente/templates/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include ActionText::Attachable
   include Polivalente::Archiver
   include Polivalente::Commentator
   include Polivalente::Reactor
